038552be5915dbb1199a88adee5d6dd508f9d03a
Anoop HM <anoophm44@gmail.com>
2019-11-15 21:59:57 +0530

    Issue #SB-15374 fix: pr review comment changes

--- sdks/UserSDK.ts
+++ sdks/UserSDK.ts
@@ -2,11 +2,10 @@ import { Singleton } from "typescript-ioc";
 import { logger } from "@project-sunbird/ext-framework-server/logger";
 import * as _ from "lodash";
 import { Inject } from "typescript-ioc";
-import { DataBaseSDK } from "./DataBaseSDK";
+import { DataBaseSDK, DBError } from "./DataBaseSDK";
 import { IUser } from "./../interfaces";
-import uuid from "uuid/v4";
-const DEFAULT_USER_ID = 'guest';
-const USER_DB = 'user';
+const DEFAULT_USER = 'guest1';
+const USER_DB = 'users';
 
 @Singleton
 export class UserSDK {
@@ -15,16 +14,25 @@ export class UserSDK {
   private dbSDK: DataBaseSDK;
   constructor() {}
 
-  public async read(_id = DEFAULT_USER_ID): Promise<IUser>{
-    return this.dbSDK.getDoc(USER_DB, _id);
+  public async read(_id = DEFAULT_USER): Promise<IUser | DBError>{
+    return this.dbSDK.getDoc(USER_DB, _id).then(res => {
+      delete res._rev;
+      return res;
+    }).catch(error => {
+      throw this.dbSDK.handleError(error);
+    });
   }
 
-  public async create(user: IUser): Promise<any>{
-    user._id = !user.name ?  DEFAULT_USER_ID : uuid();
-    user.name = user.name || DEFAULT_USER_ID;
+  public async create(user: IUser): Promise<{id: string} | DBError>{
+    user._id = !user.name ?  DEFAULT_USER : user.name.replace(/ /g,'').toLowerCase();
+    user.name = user.name || DEFAULT_USER;
     user.createdOn = Date.now();
     user.updatedOn = Date.now();
-    return this.dbSDK.insertDoc(USER_DB, user, user._id);
+    return this.dbSDK.insertDoc(USER_DB, user, user._id)
+    .then(data => ({id: data.id}))
+    .catch(error => {
+      throw this.dbSDK.handleError(error);
+    });;
   }
 
 }
