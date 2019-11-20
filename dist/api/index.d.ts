09935483714a5aeb0c603eae29e27c326479960b
Anoop HM <anoophm44@gmail.com>
2019-11-20 11:27:55 +0530

    Issue #SB-15762 feat: updating dist folder

--- dist/api/index.d.ts
+++ dist/api/index.d.ts
@@ -7,7 +7,9 @@ import FileSDK from "./../sdks/FileSDK";
 import DownloadManager from "./../managers/DownloadManager/DownloadManager";
 import SystemSDK from "./../sdks/SystemSDK";
 import TelemetrySDK from "./../sdks/TelemetrySDK";
+import { UserSDK } from "./../sdks/UserSDK";
 declare class ContainerAPI {
+    userSDK: UserSDK;
     bootstrap(): Promise<void>;
     register(pluginId: string, pluginInfo: PluginConfig): Promise<void>;
     getSettingSDKInstance(pluginId: string): SettingSDK;
@@ -16,6 +18,7 @@ declare class ContainerAPI {
     getDownloadManagerInstance(pluginId: string): DownloadManager;
     getSystemSDKInstance(pluginId: string): SystemSDK;
     getTelemetrySDKInstance(): TelemetrySDK;
+    getUserSdkInstance(): UserSDK;
 }
 export declare const containerAPI: ContainerAPI;
 export {};
