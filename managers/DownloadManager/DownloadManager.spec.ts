93550ec75d93a4d1a6a95dee8050a08d914bc15f
Harishkumar Gangula <harishg@ilimi.in>
2019-11-20 12:20:43 +0530

    Issue #SB-15854 fix: CircleCI build fix download manager

--- managers/DownloadManager/DownloadManager.spec.ts
+++ managers/DownloadManager/DownloadManager.spec.ts
@@ -9,6 +9,8 @@ let fileSDK = new FileSDK("testplugindownload");
 
 describe("DownloadManager", () => {
   before(async () => {
+    process.env.FILES_PATH = __dirname;
+    console.log(process.env.FILES_PATH)
     process.env.DATABASE_PATH = fileSDK.getAbsPath("");
   });
 
