3126a15882c03b6c22558afa9d303a06bd1c123e
Sourav Dey <sourav_d@techjoomla.com>
2020-03-11 16:58:47 +0530

    Issue #SB-17669: SDK set/get telemetry sync info

--- dist/sdks/TelemetrySDK.d.ts
+++ dist/sdks/TelemetrySDK.d.ts
@@ -2,8 +2,12 @@ import { TelemetryInstance } from "./../services/telemetry/telemetryInstance";
 export default class TelemetrySDK {
     private telemetryInstance;
     private telemetryExport;
+    private settingSDK;
+    private cb;
     getInstance(): TelemetryInstance;
     send(events: any[]): Promise<any>;
     export(destPath: string, cb: any): Promise<void>;
     info(cb: any): Promise<void>;
+    setIsTelemetrySyncToServer(syncToServer: boolean, cb: any): void;
+    getIsTelemetrySyncToServer(cb: any): Promise<void>;
 }
--- sdks/TelemetrySDK.ts
+++ sdks/TelemetrySDK.ts
@@ -1,10 +1,13 @@
 import { TelemetryInstance } from "./../services/telemetry/telemetryInstance";
 import { TelemetryExport } from './../services/telemetry/TelemetryExport';
 import { Inject } from "typescript-ioc";
+import SettingSDK from './SettingSDK'
 
 export default class TelemetrySDK {
   @Inject private telemetryInstance: TelemetryInstance;
   @Inject private telemetryExport: TelemetryExport;
+  @Inject private settingSDK: SettingSDK;
+  private cb;
 
   getInstance() {
     return this.telemetryInstance;
@@ -20,5 +23,24 @@ export default class TelemetrySDK {
 
   info(cb) {
     return this.telemetryExport.info(cb)
+  }
+
+  setIsTelemetrySyncToServer(syncToServer: boolean, cb) {
+    this.cb = cb;
+    try {
+      let respData = this.settingSDK.put('isTelemetrySyncToServer', { syncToServer: syncToServer, updatedOn: Date.now() });
+      this.cb(null, respData);
+    } catch (error) {
+      this.cb(error);
+    }
+  }
+
+  async getIsTelemetrySyncToServer(cb) {
+    this.cb = cb;
+    try {
+      this.cb(null, await this.settingSDK.get('isTelemetrySyncToServer'));
+    } catch (error) {
+      this.cb(null, { syncToServer: true });
+    }
   }
 }
